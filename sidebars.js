2eb237505dd7c525bcec3a85101a0fe86bea8b5e
zhusen <zhusen@oschina.cn>
2023-04-29 17:01:11 +0800

    init

---
+++ sidebars.js
@@ -0,0 +1,54 @@
+/**
+ * Creating a sidebar enables you to:
+ * - create an ordered group of docs
+ * - render a sidebar for each doc of that group
+ * - provide next/previous navigation.
+ *
+ * The sidebars can be generated from the filesystem, or explicitly defined here.
+ *
+ * Create as many sidebars as you want.
+ */
+
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Vue入门系列',
+      items: ['vue/01 环境搭建', 'vue/02 模版语法', 'vue/03 条件渲染和列表渲染', 'vue/04 事件处理和表单绑定', 'vue/05 组件'],
+    },
+    {
+      type: 'category',
+      label: 'Vue原理系列',
+      items: ['DocSearch-v3', 'api', 'styling', 'migrating-from-v2'],
+    },
+    {
+      type: 'category',
+      label: 'React入门系列',
+      items: ['record-extractor', 'templates', 'manage-your-crawls'],
+    },
+    {
+      type: 'category',
+      label: 'Requirements, tips, FAQ',
+      items: [
+        {
+          type: 'category',
+          label: 'FAQ',
+          items: ['crawler', 'DocSearch-program'],
+        },
+        {
+          type: 'doc',
+          id: 'tips',
+        },
+        {
+          type: 'doc',
+          id: 'integrations',
+        },
+      ],
+    },
+    {
+      type: 'category',
+      label: 'Under the hood',
+      items: ['how-does-it-work', 'required-configuration'],
+    },
+  ],
+};
